6ccd750e4973bbd92322dd3e411be0e27e3a288d
Yash728 <61221867+Yash728@users.noreply.github.com>
2020-11-30 00:13:44 +0530

    Practical19 done

---
+++ JavaScript/Practical19.js
@@ -0,0 +1,36 @@
+//Selectors
+
+const form1 = document.getElementById("form1");
+const form2 = document.getElementById("form2");
+const form3 = document.getElementById("form3");
+
+const next1 = document.getElementById("next1");
+const next2 = document.getElementById("next2");
+const back1 = document.getElementById("back1");
+const back2 = document.getElementById("back2");
+
+const progress = document.getElementById("progress");
+
+//Event Handler
+
+next1.onclick = () => {
+    form1.style.left = "-450px";
+    form2.style.left = "40px";
+    progress.style.width = "240px"
+
+}
+back1.onclick = () => {
+    form1.style.left = "40px";
+    form2.style.left = "450px";
+    progress.style.width = "120px"
+}
+next2.onclick = () => {
+    form2.style.left = "-450px";
+    form3.style.left = "40px";
+    progress.style.width = "360px"
+}
+back2.onclick = () => {
+    form2.style.left = "40px";
+    form3.style.left = "450px";
+    progress.style.width = "240px"
+}
